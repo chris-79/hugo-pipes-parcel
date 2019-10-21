bfd5bace7643842cf6728d9a07a769537c878e29
Chris S <chris@chris79.com>
2019-10-21 15:26:00 -0400

    Switches fonts to our fonts

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,7 @@
 // To use https://github.com/KyleAMathews/typefaces
 import "./css/styles.css";
-import "typeface-roboto"
+import "typeface-merriweather"
+import "typeface-merriweather-sans"
+import "typeface-oswald"
 
 import "./js/nojs.js"
